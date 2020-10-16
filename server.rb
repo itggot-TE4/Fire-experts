df62944fd770836c3efa93be05986a976c2f64ab
te4-adrian-almetun-smeds <adrianalmetunsmeds@hotmail.com>
2020-10-16 22:10:18 +0200

    Implementation for gh login

--- server.rb
+++ server.rb
@@ -8,9 +8,11 @@ Dotenv.load
 
 # Webserver handler
 class Server < Sinatra::Base
+  enable :sessions
+
   # Serves the page
   get '/' do
-    return File.read('./public/index.html')
+    slim :index
   end
 
   get '/api/github/:name/repos' do
@@ -22,12 +24,41 @@ class Server < Sinatra::Base
               "#{params['repo']}/forks").body
   end
 
+  get '/api/github/user' do
+    response = call_gh_api('https://api.github.com/user')
+    if response.parsed_response == response.body
+      session[:access_token] = nil
+      return ''
+    else
+      response.body
+    end
+  end
+
+  get '/callback' do
+    begin
+      # get temporary GitHub code...
+      session_code = request.env['rack.request.query_hash']['code']
+      # ... and POST it back to GitHub
+      result = HTTParty.post('https://github.com/login/oauth/access_token',
+                             { body:
+                               { client_id: ENV['GH_BASIC_CLIENT_ID'],
+                                 client_secret: ENV['GH_BASIC_SECRET_ID'],
+                                 code: session_code },
+                               headers: { accept: 'application/json' } })
+      session[:access_token] = JSON.parse(result.body)['access_token']
+    rescue StandardError
+      session[:access_token] = nil
+    ensure
+      redirect '/'
+    end
+  end
+
   def call_gh_api(url)
-    auth = {
-      username: ENV['GH_USER_NAME'],
-      token: ENV['GH_ACCESS_TOKEN']
-    }
-    options = { basic_auth: auth }
-    HTTParty.get(url, options)
+    auth = "Bearer #{session[:access_token]}"
+    HTTParty.get(url, headers: { 'Authorization' => auth })
+  end
+
+  def authorized?
+    return if session[:access_token]
   end
 end
